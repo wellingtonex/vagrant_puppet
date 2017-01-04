be5ccc4301728078041d3b83e839edc5f68428c5
wellington.siqueira <wellingtonex@gmail.com>
2017-01-04 18:45:30 -0300

    provisionando o Mysql

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,7 +1,7 @@
 exec { "apt-update":
     command => "/usr/bin/apt-get update"
 }
-package { ["openjdk-7-jre", "tomcat7"]:
+package { ["openjdk-7-jre", "tomcat7", "mysql-server"]:
     ensure => installed,
     require => Exec["apt-update"]
 }
@@ -12,6 +12,21 @@ service { "tomcat7":
     hasstatus => true,
     hasrestart => true,
     require => Package["tomcat7"]    
+}
+
+service { "mysql":
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true,
+    require => Package["mysql-server"]
+}
+
+exec { "musicjungle":
+    command => "mysqladmin -uroot create musicjungle",
+    unless => "mysql -u root musicjungle",
+    path => "/usr/bin",
+    require => Service["mysql"]
 }
 
 file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
